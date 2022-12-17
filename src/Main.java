099e4e4f07311abb60d1b298abe5a256a614a6a7
wilzzadex <a.waliyuddin@kabayan.id>
2022-12-17 22:53:06 +0700

    Tugas Struktur data Queue

--- src/Main.java
+++ src/Main.java
@@ -3,45 +3,48 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the size of the queue: ");
+        System.out.print("Masukkan jumlah queue: ");
         int size = scanner.nextInt();
         Queue queue = new Queue(size);
         while (true) {
-            System.out.println("1. Enqueue");
-            System.out.println("2. Dequeue");
-            System.out.println("3. Peek");
-            System.out.println("4. Size");
-            System.out.println("5. Is empty?");
-            System.out.println("6. Is full?");
-            System.out.println("7. Exit");
-            System.out.print("Enter your choice: ");
+            System.out.println("1. Masukan queue");
+            System.out.println("2. Keluarkan queue");
+            System.out.println("3. Data paling depan");
+            System.out.println("4. Jumlah data");
+            System.out.println("5. Cek kosong ?");
+            System.out.println("6. Cek penuh ?");
+            System.out.println("7. Print Queue");
+            System.out.println("8. Keluar");
+            System.out.print("Pilih menu: ");
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.print("Enter the data: ");
+                    System.out.print("Masukkan data: ");
                     int data = scanner.nextInt();
                     queue.enqueue(data);
                     break;
                 case 2:
-                    System.out.println("Dequeued data: " + queue.dequeue());
+                    System.out.println("Keluarkan queue " + queue.dequeue());
                     break;
                 case 3:
-                    System.out.println("Peeked data: " + queue.peek());
+                    System.out.println("Data paling depan " + queue.peek());
                     break;
                 case 4:
-                    System.out.println("Size of the queue: ");
+                    System.out.println("Size Queue: " + queue.size());
                     break;
                 case 5:
-                    System.out.println("Is the queue empty? " + queue.isEmpty());
+                    System.out.println("Apakah queue kosong? " + queue.isEmpty());
                     break;
                 case 6:
-                    System.out.println("Print queue \n");
-                    queue.printQueue();
+                    System.out.println("Apakah queue penuh? " + queue.isFull());
                     break;
                 case 7:
+                    System.out.println("Print Queue: ");
+                    queue.printQueue();
+                case 8:
                     System.exit(0);
                 default:
-                    System.out.println("Invalid choice");
+                    System.out.println("Pilihan tidak ada");
 
             }
         }
--- src/Queue.java
+++ src/Queue.java
@@ -21,7 +21,7 @@ public class Queue {
 
     public void enqueue(int data) {
         if (isFull()) {
-            System.out.println("Queue is full");
+            System.out.println("Queue sudah penuh");
         } else {
             rear = (rear + 1) % capacity;
             queue[rear] = data;
@@ -31,7 +31,7 @@ public class Queue {
 
     public int dequeue() {
         if (isEmpty()) {
-            System.out.println("Queue is empty");
+            System.out.println("Queue kosong");
             return -1;
         } else {
             int data = queue[front];
@@ -43,7 +43,7 @@ public class Queue {
 
     public int peek() {
         if (isEmpty()) {
-            System.out.println("Queue is empty");
+            System.out.println("Queue kosong");
             return -1;
         } else {
             return queue[front];
@@ -64,60 +64,13 @@ public class Queue {
 
     // print array queue
     public void printQueue() {
-        System.out.print("Queue: ");
+        System.out.print("List Queue: ");
         for (int i = 0; i < size; i++) {
             System.out.print(queue[(front + i) % capacity] + " ");
         }
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the size of the queue: ");
-        int size = scanner.nextInt();
-        Queue queue = new Queue(size);
-        while (true) {
-            System.out.println("1. Enqueue");
-            System.out.println("2. Dequeue");
-            System.out.println("3. Peek");
-            System.out.println("4. Size");
-            System.out.println("5. Is empty?");
-            System.out.println("6. Is full?");
-            System.out.println("7. Exit");
-            System.out.print("Enter your choice: ");
-            int choice = scanner.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter the data: ");
-                    int data = scanner.nextInt();
-                    queue.enqueue(data);
-                    break;
-                case 2:
-                    System.out.println("Dequeued data: " + queue.dequeue());
-                    break;
-                case 3:
-                    System.out.println("Peeked data: " + queue.peek());
-                    break;
-                case 4:
-                    System.out.println("Size of the queue: " + queue.size());
-                    break;
-                case 5:
-                    System.out.println("Is the queue empty? " + queue.isEmpty());
-                    break;
-                case 6:
-                    System.out.println("Is the queue full? " + queue.isFull());
-                    break;
-                case 7:
-                    System.out.println("Print Queue: ");
-                    queue.printQueue();
-                case 8:
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid choice");
 
-            }
-        }
-
-    }
 
 }
